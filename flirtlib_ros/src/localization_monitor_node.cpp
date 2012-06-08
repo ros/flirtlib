23ee19cb8d2630a5f42abc34b63a61839e7ec33b
Bhaskara Marthi <bhaskara@gmail.com>
2012-06-08 16:28:33 -0700

    Get localization node saving scans when possible

--- flirtlib_ros/src/localization_monitor_node.cpp
+++ flirtlib_ros/src/localization_monitor_node.cpp
@@ -37,9 +37,12 @@
  */
 
 #include <flirtlib_ros/localization_monitor.h>
+#include <flirtlib_ros/conversions.h>
 #include <boost/thread.hpp>
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
+#include <mongo_ros/message_collection.h>
+#include <visualization_msgs/Marker.h>
 
 namespace flirtlib_ros
 {
@@ -47,8 +50,17 @@ namespace flirtlib_ros
 namespace sm=sensor_msgs;
 namespace gm=geometry_msgs;
 namespace nm=nav_msgs;
+namespace mr=mongo_ros;
+namespace vm=visualization_msgs;
+
+using std::string;
+using std::vector;
+using boost::format;
+using boost::shared_ptr;
 
 typedef boost::mutex::scoped_lock Lock;
+typedef mr::MessageWithMetadata<RefScanRos>::ConstPtr DBScan;
+typedef vector<InterestPoint*> InterestPointVec;
 
 class Node
 {
@@ -56,28 +68,81 @@ public:
 
   Node ();
 
-  void scanCB (const sm::LaserScan& scan);
+  void scanCB (sm::LaserScan::ConstPtr scan);
   
   void mapCB (const nm::OccupancyGrid& g);
-
+  
 private:
+  
+  gm::Pose getPose ();
+  RefScanRos extractFeatures (sm::LaserScan::ConstPtr scan,
+                              const gm::Pose& pose) const;
 
+  void updateLocalized (sm::LaserScan::ConstPtr scan);
+  void updateUnlocalized ();
+  
+  // Needed during init
   boost::mutex mutex_;
   ros::NodeHandle nh_;
+  
+  // Flirtlib objects
+  shared_ptr<SimpleMinMaxPeakFinder> peak_finder_;
+  shared_ptr<HistogramDistance<double> > histogram_dist_;
+  shared_ptr<Detector> detector_;
+  shared_ptr<DescriptorGenerator> descriptor_;
+  shared_ptr<RansacFeatureSetMatcher> ransac_;
 
-  boost::shared_ptr<ScanPoseEvaluator> evaluator_;
+  shared_ptr<ScanPoseEvaluator> evaluator_;
+  bool previously_matched_;
 
+  mr::MessageCollection<RefScanRos> scans_;
   tf::TransformListener tf_;
   ros::Subscriber scan_sub_;
   ros::Subscriber map_sub_;
+  ros::Publisher marker_pub_;
 };
 
 
-// Constructor sets up ros comms
+Detector* createDetector (SimpleMinMaxPeakFinder* peak_finder)
+{
+  const double scale = 5.0;
+  const double dmst = 2.0;
+  const double base_sigma = 0.2;
+  const double sigma_step = 1.4;
+  CurvatureDetector* det = new CurvatureDetector(peak_finder, scale, base_sigma,
+                                                 sigma_step, dmst);
+  det->setUseMaxRange(false);
+  return det;
+}
+
+DescriptorGenerator* createDescriptor (HistogramDistance<double>* dist)
+{
+  const double min_rho = 0.02;
+  const double max_rho = 0.5;
+  const double bin_rho = 4;
+  const double bin_phi = 12;
+  BetaGridGenerator* gen = new BetaGridGenerator(min_rho, max_rho, bin_rho,
+                                                 bin_phi);
+  gen->setDistanceFunction(dist);
+  return gen;
+}
+
+// Constructor sets up ros comms and flirtlib objects
 Node::Node () :
+  peak_finder_(new SimpleMinMaxPeakFinder(0.34, 0.001)),
+  histogram_dist_(new SymmetricChi2Distance<double>()),
+  detector_(createDetector(peak_finder_.get())),
+  descriptor_(createDescriptor(histogram_dist_.get())),
+  ransac_(new RansacFeatureSetMatcher(0.0599, 0.95, 0.4, 0.4,
+                                      0.0384, false)),
+  previously_matched_(false),
+  scans_("flirtlib_place_rec", "scans", "mongo.willowgarage.com"),
   scan_sub_(nh_.subscribe("base_scan", 10, &Node::scanCB, this)),
-  map_sub_(nh_.subscribe("map", 10, &Node::mapCB, this))
-{}
+  map_sub_(nh_.subscribe("map", 10, &Node::mapCB, this)),
+  marker_pub_(nh_.advertise<vm::Marker>("visualization_marker", 10))
+{
+  ROS_INFO("Localization monitor initialized");
+}
 
 
 void Node::mapCB (const nm::OccupancyGrid& g)
@@ -96,16 +161,78 @@ gm::Pose toMsg (const tf::Transform& trans)
   return m;
 }
 
+gm::Pose Node::getPose ()
+{
+  tf::StampedTransform trans;
+  tf_.lookupTransform("/map", "base_laser_link", ros::Time(), trans);
+  gm::Pose pose;
+  tf::poseTFToMsg(trans, pose);
+  return pose;
+}
+
+RefScanRos Node::extractFeatures (sm::LaserScan::ConstPtr scan,
+                                  const gm::Pose& pose) const
+{
+  shared_ptr<LaserReading> reading = fromRos(*scan);
+  InterestPointVec pts;
+  detector_->detect(*reading, pts);
+  BOOST_FOREACH (InterestPoint* p, pts) 
+    p->setDescriptor(descriptor_->describe(*p, *reading));
+  marker_pub_.publish(interestPointMarkers(pts, pose));
+  const RefScan ref(scan, pose, pts);
+  return toRos(ref);
+}
+
+
+
+void Node::updateUnlocalized ()
+{
+    ROS_INFO("Not well localized");
+    if (previously_matched_)
+      ROS_INFO("Previously matched, so not doing anything");
+    else
+    {
+      ROS_INFO("Here's where I'd try to match this scan");
+    }
+}
+
+void Node::updateLocalized (sm::LaserScan::ConstPtr scan)
+{
+    ROS_INFO("Well localized");
+    const double DPOS = 0.5;
+    const double DTHETA = 0.5;
+  
+    gm::Pose current = getPose();
+    const double x = current.position.x;
+    const double y = current.position.y;
+    const double th = tf::getYaw(current.orientation);
+    format f("{x : {$gt: %.4f, $lt: %.4f}, y : {$gt: %.4f, $lt: %.4f}, "
+             "theta: {$gt: %.4f, $lt: %.4f} }");
+    string str = (f % (x-DPOS) % (x+DPOS) % (y-DPOS) % (y+DPOS) %
+                  (th-DTHETA) % (th+DTHETA)).str();
+    mongo::Query q = mongo::fromjson(str);
+    
+    vector<DBScan> scans = scans_.pullAllResults(q, true);
+    ROS_INFO ("Found %zu nearby scans", scans.size());
+    if (scans.size()<1)
+    {
+      scans_.insert(extractFeatures(scan, current),
+                    mr::Metadata("x", x, "y", y, "theta", th));
+      ROS_DEBUG_NAMED ("save_scan", "Saved scan at %.2f, %.2f, %.2f", x, y, th);
+    }
+}
+
+
 // Given current scan and pose, evaluate how good our localization is
-void Node::scanCB (const sm::LaserScan& scan)
+void Node::scanCB (sm::LaserScan::ConstPtr scan)
 {
   // First, look up the sensor pose at the time of the scan
-  tf_.waitForTransform("/map", scan.header.frame_id, scan.header.stamp,
+  tf_.waitForTransform("/map", scan->header.frame_id, scan->header.stamp,
                        ros::Duration(0.5));
   tf::StampedTransform trans;
   try
   {
-    tf_.lookupTransform("/map", scan.header.frame_id, scan.header.stamp,
+    tf_.lookupTransform("/map", scan->header.frame_id, scan->header.stamp,
                         trans);
   }
   catch (tf::TransformException& e)
@@ -115,17 +242,21 @@ void Node::scanCB (const sm::LaserScan& scan)
   }
   
   // Have we initialized the evaluator yet?
-    Lock l(mutex_);
-    if (!evaluator_)
-    {
-      ROS_INFO_STREAM ("Skipping scan as evaluator not yet initialized");
-      return;
-    }
+  Lock l(mutex_);
+  if (!evaluator_)
+  {
+    ROS_INFO_STREAM ("Skipping scan as evaluator not yet initialized");
+    return;
+  }
     
   // Evaluate how well this scan is localized
-  const double dist = (*evaluator_)(scan, toMsg(trans));
-  ROS_INFO_THROTTLE (1.0, "Median distance is %.4f", dist);
-                           
+  const double dist = (*evaluator_)(*scan, toMsg(trans));
+  //ROS_INFO_THROTTLE (1.0, "Median distance is %.4f", dist);
+  
+  if (dist < 0.1)
+    updateLocalized(scan);
+  else
+    updateUnlocalized();
 }
 
 } // namespace
